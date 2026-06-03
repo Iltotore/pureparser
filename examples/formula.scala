fcfad3ef61b95a4b4ea299bdd9d57699a58cd5e9
Raphaël FROMENTIN <rafbodaha@gmail.com>
2026-06-03 17:03:47 +0200

    docs: Add example parsers

---
+++ examples/formula.scala
@@ -0,0 +1,79 @@
+package io.github.iltotore.pureparser.json
+
+import io.github.iltotore.pureparser.*
+import purelogic.*
+import scala.io.Source
+import scala.util.Using
+
+enum Expr:
+  case Invalid(span: Span)
+  case Literal(value: Double, span: Span)
+  case Add(left: Expr, right: Expr, span: Span)
+  case Sub(left: Expr, right: Expr, span: Span)
+  case Mul(left: Expr, right: Expr, span: Span)
+  case Div(left: Expr, right: Expr, span: Span)
+
+  def span: Span
+
+val literalParser: Parser[Char, Expr] = Expr.Literal.apply.tupled(
+  Parser.span(
+    Parser.regex("[0-9]+(.[0-9]+)?")
+      .toDoubleOption
+      .getOrElse(Parser.abort)
+  )
+)
+
+val parenthesizedParser: Parser[Char, Expr] = Parser.inOrder(
+  Parser.literal('('),
+  exprParser,
+  Parser.literal(')')
+)
+
+val termParser: Parser[Char, Expr] = Parser.recoverWith(
+  Parser.expect(Parser.firstOf(literalParser, parenthesizedParser), "Literal or parenthesized expression"),
+  RecoverStrategy.firstOf(
+    RecoverStrategy.nestedDelimiters('(', ')', Expr.Invalid(Span(0, 0))),
+    RecoverStrategy.skipUntil(Parser.firstOf(Parser.oneOf("+-*/)"), Parser.eof), Expr.Invalid(Span(0, 0)))
+  )
+)
+
+val binaryAddOps: Map[Char, (Expr, Expr, Span) => Expr] = Map(
+  '+' -> Expr.Add.apply,
+  '-' -> Expr.Sub.apply
+)
+
+val binaryMulOps: Map[Char, (Expr, Expr, Span) => Expr] = Map(
+  '*' -> Expr.Mul.apply,
+  '/' -> Expr.Div.apply
+)
+
+val allOperators = binaryAddOps.keySet ++ binaryMulOps.keySet
+
+def binaryOpsParser(operandParser: Parser[Char, Expr], operators: Map[Char, (Expr, Expr, Span) => Expr], isMostPrecedence: Boolean): Parser[Char, Expr] =
+  Parser.separatedByReduce(
+    operandParser,
+    locally:
+      val operatorKeys = operators.keySet
+      val operator = Parser.recoverWith(
+        operators(Parser.spaced(Parser.oneOf(operatorKeys))),
+        RecoverStrategy.skipThenRetryUntil(Parser.oneOf(allOperators -- operatorKeys + ')'))
+      )
+      (left, right) => operator(left, right, left.span.merge(right.span))
+  )
+
+val binaryMulParser: Parser[Char, Expr] = binaryOpsParser(termParser, binaryMulOps, true)
+val binaryAddParser: Parser[Char, Expr] = binaryOpsParser(binaryMulParser, binaryAddOps, false)
+
+val exprParser: Parser[Char, Expr] = Parser.recoverWith(
+  Parser.expect(Parser.spaced(binaryAddParser), "Valid expression"),
+  RecoverStrategy.firstOf(
+    RecoverStrategy.nestedDelimiters('(', ')', Expr.Invalid(Span(0, 0))),
+    RecoverStrategy.skipUntil(Parser.firstOf(Parser.oneOf(")"), Parser.eof), Expr.Invalid(Span(0, 0)))
+  )
+)
+
+@main
+def run(path: String): Unit =
+  Using(Source.fromFile(path))(source =>
+    println(pprint(Parser(source.mkString)(exprParser)))
+  ).fold(throw _, identity)
---
+++ examples/json.scala
@@ -0,0 +1,111 @@
+package io.github.iltotore.pureparser.json
+
+import io.github.iltotore.pureparser.*
+import purelogic.*
+import scala.io.Source
+import scala.util.Using
+
+enum Json derives CanEqual:
+  case JInvalid
+  case JNull(span: Span)
+  case JBool(value: Boolean, span: Span)
+  case JInt(value: Int, span: Span)
+  case JString(value: String, span: Span)
+  case JArray(elements: List[Json], span: Span)
+  case JObject(fields: Map[String, Json], span: Span)
+
+val nullParser: Parser[Char, Json] = Json.JNull(Parser.span(Parser.literal("null")))
+
+val booleanParser: Parser[Char, Json] = Parser.firstOf(
+  Json.JBool(true, Parser.span(Parser.literal("true"))),
+  Json.JBool(false, Parser.span(Parser.literal("false")))
+)
+
+val intParser: Parser[Char, Json] = Json.JInt.apply.tupled(
+  Parser.span:
+    val start = get
+    Parser.regex("[+-]?[0-9]+")
+      .toIntOption
+      .getOrElse(fail(()))
+)
+
+val rawStringParser: Parser[Char, String] = Parser.inOrder(
+  Parser.literal('"'),
+  Parser.regex("[^\"]+"),
+  Parser.literal('"')
+)
+
+val stringParser: Parser[Char, Json] = Json.JString.apply.tupled(Parser.span(rawStringParser))
+
+val arrayParser: Parser[Char, Json] = Json.JArray.apply.tupled(
+  Parser.span(
+    Parser.inOrder(
+      Parser.literal('['),
+      Parser.separatedByUntil(
+        jsonParser,
+        Parser.spaced(Parser.literal(',')),
+        Parser.literal(']')
+      ),
+      Parser.literal(']')
+    )
+  )
+)
+
+val fieldParser: Parser[Char, (String, Json)] = Parser.spaced(
+  Parser.inOrder(
+    rawStringParser,
+    Parser.spaced(Parser.literal(':')),
+    jsonParser
+  )
+)
+
+val objectParser: Parser[Char, Json] = Json.JObject.apply.tupled(
+  Parser.span(
+    Parser.inOrder(
+      Parser.literal('{'),
+      Parser.separatedByUntil(
+        fieldParser,
+        Parser.spaced(Parser.literal(',')),
+        Parser.literal('}')
+      ).toMap,
+      Parser.literal('}')
+    )
+  )
+)
+
+val jsonParser: Parser[Char, Json] = Parser.recoverWith(
+  Parser.expect(
+    Parser.spaced(
+      Parser.firstOf(
+        objectParser,
+        arrayParser,
+        nullParser,
+        booleanParser,
+        intParser,
+        stringParser
+      )
+    ),
+    "JSON expression"
+  ),
+  RecoverStrategy.firstOf(
+    RecoverStrategy.nestedDelimiters(
+      '[',
+      ']',
+      Json.JInvalid,
+      ('{', '}')
+    ),
+    RecoverStrategy.nestedDelimiters(
+      '{',
+      '}',
+      Json.JInvalid,
+      ('[', ']')
+    ),
+    RecoverStrategy.skipUntil(Parser.oneOf(",]}"), Json.JInvalid)
+  )
+)
+
+@main
+def run(path: String): Unit =
+  Using(Source.fromFile(path))(source =>
+    println(pprint(Parser(source.mkString)(jsonParser)))
+  ).fold(throw _, identity)
